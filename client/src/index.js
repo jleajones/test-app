2fccf661bf302374cc5366eba74a3356bf9f43a4
Jason Lea-Jones <jleajones@gmail.com>
2018-04-10 18:27:15 -0400

    semantic ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
-import App from "./component/App";
+import "semantic-ui-css/semantic.min.css";
+import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
