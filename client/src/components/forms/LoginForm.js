eb2e3cf1d833244708147f911637709ee82dbd86
Jason Lea-Jones <jleajones@gmail.com>
2018-04-10 20:51:01 -0400

    created loginForm

---
+++ client/src/components/forms/LoginForm.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Form, Button, Message, Label, Input } from "semantic-ui-react";
+import Validator from "validator";
+
+const { keys } = Object;
+
+class LoginForm extends Component {
+  state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
+    errors: {}
+  };
+
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+  onSubmit = () => {
+    const { data } = this.state;
+    const errors = this.validate(data);
+
+    if (keys(errors).length === 0) this.props.submit(data);
+
+    this.setState({
+      errors
+    });
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email))
+      errors.email = "Please enter a valid email address";
+    if (!data.password) errors.password = "Password cannot be blank";
+
+    return errors;
+  };
+
+  render() {
+    const { data, errors } = this.state;
+    const errorsList = keys(errors).map(key => errors[key]);
+
+    return (
+      <Form onSubmit={this.onSubmit} error={errors.password || errors.email}>
+        <Message
+          error
+          header="Oops, looks like there was mistake."
+          list={errorsList}
+        />
+        <Form.Field error={!!errors.email}>
+          <label htmlFor="email">
+            Email:
+            <Input
+              id="email"
+              type="text"
+              onChange={this.onChange}
+              placeholder="enter your email"
+              name="email"
+              value={data.email}
+            />
+          </label>
+          {errors.email && (
+            <Label basic color="red" pointing>
+              {errors.email}
+            </Label>
+          )}
+        </Form.Field>
+        <Form.Field error={!!errors.password}>
+          <label htmlFor="password">
+            Password:
+            <Input
+              id="password"
+              type="password"
+              onChange={this.onChange}
+              placeholder="enter your password"
+              name="password"
+              value={data.password}
+            />
+          </label>
+          {errors.password && (
+            <Label basic color="red" pointing>
+              {errors.password}
+            </Label>
+          )}
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
+
+export default LoginForm;
--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,5 +1,19 @@
 import React from "react";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => <div>Login Page</div>;
+class LoginPage extends React.Component {
+  onSubmit = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <LoginForm submit={this.onSubmit} />
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
