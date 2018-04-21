63a3c09f8b7dc64b9cfd693360ada4099b6a0f91
Jason Lea-Jones <jleajones@gmail.com>
2018-04-20 20:41:23 -0400

    redux setup;

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-import App from "./components/App";
+
 import registerServiceWorker from "./registerServiceWorker";
+
+import store from "./store";
+import App from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
