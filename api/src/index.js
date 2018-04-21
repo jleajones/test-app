857bb11d208be40e77734a970c2f40f6f7b8920f
Jason Lea-Jones <jleajones@gmail.com>
2018-04-20 23:49:05 -0400

    updates for login api

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyPaser from "body-parser";
+import dotenv from "dotenv";
+import Promise from "bluebird";
 
+import auth from "./routes/auth";
+
+dotenv.config();
 const app = express();
+app.use(bodyPaser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL);
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { server: "Invalid Credentials" } });
-});
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
